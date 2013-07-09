41ca26eba053f77d9a5c7a8d2500066094020997
Ubuntu <ubuntu@ip-172-31-40-111.us-west-2.compute.internal>
2013-07-09 11:46:41 +0000

    add index.html

--- web.js
+++ web.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var fs = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
@@ -10,3 +10,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
